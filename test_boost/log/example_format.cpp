e98533c7769d627e9aa0a85f71116ab7d7b1ae5f
root <root@AY14020823153388370fZ.(none)>
2016-04-06 20:02:44 +0800

    add format

--- test_boost/log/example_format.cpp
+++ test_boost/log/example_format.cpp
@@ -1,9 +1,5 @@
-/*
- *          Copyright Andrey Semashev 2007 - 2015.
- * Distributed under the Boost Software License, Version 1.0.
- *    (See accompanying file LICENSE_1_0.txt or copy at
- *          <a href="http://www.boost.org/LICENSE_1_0.txt">http://www.boost.org/LICENSE_1_0.txt</a>)
- */
+
+#include <fstream>
 
 #include <boost/date_time/posix_time/posix_time_types.hpp>
 #include <boost/log/trivial.hpp>
@@ -14,24 +10,24 @@
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/support/date_time.hpp>
 
+#include <boost/log/sinks/sync_frontend.hpp>
+#include <boost/log/sinks/text_ostream_backend.hpp>
+
 namespace logging = boost::log;
+namespace sinks = boost::log::sinks;
 namespace src = boost::log::sources;
 namespace expr = boost::log::expressions;
+namespace attrs = boost::log::attributes;
 namespace keywords = boost::log::keywords;
 
-#if 0
-
 //[ example_tutorial_formatters_stream
-void init()
-{
-    logging::add_file_log
-    (
+void init1() {
+    logging::add_file_log (
         keywords::file_name = "sample_%N.log",
         // This makes the sink to write log records that look like this:
         // 1: <normal> A normal severity message
         // 2: <error> An error severity message
-        keywords::format =
-        (
+        keywords::format = (
             expr::stream
                 << expr::attr< unsigned int >("LineID")
                 << ": <" << logging::trivial::severity
@@ -41,19 +37,32 @@ void init()
 }
 //]
 
-#else
+void init2() {
+    typedef sinks::synchronous_sink< sinks::text_ostream_backend > text_sink;
+    boost::shared_ptr< text_sink > sink = boost::make_shared< text_sink >();
+
+    sink->locked_backend()->add_stream (
+        boost::make_shared< std::ofstream >("sample.log"));
+
+    sink->set_formatter (
+        expr::stream
+        // line id will be written in hex, 8-digits, zero-filled
+        << std::hex << std::setw(8) << std::setfill('0') << expr::attr< unsigned int >("LineID")
+        << ": <" << logging::trivial::severity
+        << "> " << expr::smessage
+    );
+
+    logging::core::get()->add_sink(sink);
+}
 
 //[ example_tutorial_formatters_stream_date_time
-void init()
-{
-    logging::add_file_log
-    (
+void init3() {
+    logging::add_file_log (
         keywords::file_name = "sample_%N.log",
         // This makes the sink to write log records that look like this:
         // YYYY-MM-DD HH:MI:SS: <normal> A normal severity message
         // YYYY-MM-DD HH:MI:SS: <error> An error severity message
-        keywords::format =
-        (
+        keywords::format = (
             expr::stream
                 << expr::format_date_time< boost::posix_time::ptime >("TimeStamp", "%Y-%m-%d %H:%M:%S")
                 << ": <" << logging::trivial::severity
@@ -63,11 +72,11 @@ void init()
 }
 //]
 
-#endif
 
-int main(int, char*[])
-{
-    init();
+
+
+int main(int, char*[]) {
+    init2();
     logging::add_common_attributes();
 
     using namespace logging::trivial;
