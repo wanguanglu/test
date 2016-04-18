d9b035252890f07e3d39c57513a4821f5d68844b
root <root@AY14020823153388370fZ.(none)>
2016-04-18 22:48:05 +0800

    add test_define.cpp

---
+++ test_c/test_define.cpp
@@ -0,0 +1,19 @@
+/**
+* @file   test.cpp
+* @author wanguanglu(wanguanglu@qq.com)
+* @date   2016/04/18 14:53:12
+* @brief 
+*  
+**/
+
+#define LOG std::cout<<__FILE__<<":"<<__LINE__<<"\t"
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, int argv[]) {
+    LOG<<"test log"<<endl;    
+    LOG<<"test log2"<<endl;
+}
