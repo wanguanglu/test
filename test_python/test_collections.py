b6827cb36104bb1fe7bee6a75c1f1eed1a53c79a
root <root@AY14020823153388370fZ.(none)>
2016-09-03 20:56:37 +0800

    update deque

--- test_python/test_collections.py
+++ test_python/test_collections.py
@@ -1,4 +1,5 @@
-
+import sys
+import time
 import collections
 
 def test_counter():
@@ -6,6 +7,29 @@ def test_counter():
     counter=collections.Counter(a)
     print counter.most_common(2)
 
+def test_deque():
+    test = collections.deque()
+
+    test.append('a')
+    test.append('b')
+    test.appendleft('c')
+
+    print test
+    print test.count('a')
+
+    print test.pop()
+    print test.popleft()
+
+    
+    fancy_loading = collections.deque('>--------------------')
+    
+    while True:
+        print '\r%s' % ''.join(fancy_loading),
+        fancy_loading.rotate(1)
+        sys.stdout.flush()
+        time.sleep(0.08)
+
 
 if __name__ == '__main__':
     test_counter()
+    test_deque()
