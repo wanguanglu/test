8bfda39229701b4696298d343971ce99343b9a56
wanguanglu <mail@wanguanglu.com>
2017-05-25 17:06:31 +0800

    add test eigen files

---
+++ test_eigen/set_size_at_run.cpp
@@ -0,0 +1,22 @@
+/**
+* @file   set_size_at_run.cpp
+* @author wanguanglu(mail@wanguanglu.com)
+* @date   2017/05/25 17:05:26
+* @brief 
+*  
+**/
+
+#include <iostream>
+#include <Eigen/Dense>
+using namespace Eigen;
+using namespace std;
+
+int main() {
+  MatrixXd m = MatrixXd::Random(3,3);
+  m = (m + MatrixXd::Constant(3,3,1.2)) * 50;
+  cout << "m =" << endl << m << endl;
+
+  VectorXd v(3);
+  v << 1, 2, 3;
+  cout << "m * v =" << endl << m * v << endl;
+}
