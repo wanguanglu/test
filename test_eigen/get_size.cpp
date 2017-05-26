85c9a1802017837518107ee04ef5c7d77edd2dcd
wanguanglu <mail@wanguanglu.com>
2017-05-26 14:03:17 +0800

    add test_eigen

--- test_eigen/get_size.cpp
+++ test_eigen/get_size.cpp
@@ -14,9 +14,14 @@ using namespace std;
 int main() {
   MatrixXd m = MatrixXd::Random(3,3);
   m = (m + MatrixXd::Constant(3, 3, 1.2)) * 50;
-  cout << "m =" << endl << m << endl;
 
   VectorXd v(3);
-  v << 1, 2, 3;
-  cout << "m * v =" << endl << m * v << endl;
+
+  cout <<"m.size: " << m.size() << endl;
+  cout <<"m.rows: " << m.rows() << endl;
+  cout <<"m.cols: " << m.cols() << endl;
+
+  cout <<"v.size: " << v.size() << endl;
+  cout <<"v.rows: " << v.rows() << endl;
+  cout <<"v.cols: " << v.cols() << endl;
 }
