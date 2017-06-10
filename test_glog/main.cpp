3f88c28eabbe0e3de4a86ea84bc374a335317274
wanguanglu <mail@wanguanglu.com>
2017-06-11 00:49:14 +0800

    add test glog.

---
+++ test_glog/main.cpp
@@ -0,0 +1,23 @@
+/**
+* @file   main.cpp
+* @author wanguanglu(mail@wanguanglu.com)
+* @date   2017/06/11 00:43:07
+* @brief 
+*  
+**/
+
+#include <glog/logging.h>
+
+void BasicTest() {
+  LOG(INFO) << "INFO log";
+  LOG(WARNING) << "WARNING log";
+  LOG(ERROR) << "ERROR log";
+  LOG(FATAL) << "FATAL log";
+}
+
+int main(int argc, char* argv[]) {
+//  google::InitGoogleLogging(argv[0]);
+  BasicTest();
+
+  return 0;
+}
