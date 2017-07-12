1ffa85a29255429957c1eb1e947e70540f8f6b48
root <mail@wanguanglu.com>
2017-07-13 01:40:08 +0800

    update helloword.py

--- test_tensorflow/helloworld.py
+++ test_tensorflow/helloworld.py
@@ -1,25 +1,30 @@
 import tensorflow as tf
 import numpy as np
 
+print "Compute with log device placement"
 with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
-    with tf.device("/gpu:1"):
+    with tf.device("/cpu:0"):
         matrix1 = tf.constant([[3., 3.]])
         matrix2 = tf.constant([[2.],[2.]])
         product = tf.matmul(matrix1, matrix2)
     result = sess.run(product)
     print result
     print product.eval()
+    print
 
+print "Compute without log device placement"
 with tf.Session() as sess:
-    with tf.device("/gpu:1"):
+    with tf.device("/cpu:0"):
         matrix1 = tf.constant([[3., 3.]])
         matrix2 = tf.constant([[2.],[2.]])
         product = tf.matmul(matrix1, matrix2)
     result = sess.run(product)
     print result
     print product.eval()
+    print
 
 
+print "Compute by InteractiveSession"
 sess = tf.InteractiveSession()
 
 x = tf.constant([[3., 3.]])
