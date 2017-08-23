873eee7c33c80ec351d02cc9ca36ad967c591653
wanguanglu <wanguanglu@didichuxing.com>
2017-08-23 23:29:54 +0800

    add test_init.

---
+++ test_c11/test_init.cpp
@@ -0,0 +1,21 @@
+/**
+* @file   test_init.cpp
+* @author wanguanglu(mail@wanguanglu.com)
+* @date   2017/08/23 23:20:39
+* @brief 
+*  
+**/
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    int a = {};
+    std::cout << a << std::endl;
+
+    int b = {};
+    std::cout << "b: " << b << std::endl;
+
+    double x = {};
+    std::cout << "x: " << x << std::endl;
+    return 0;
+}
