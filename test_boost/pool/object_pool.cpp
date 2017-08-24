4d7db34a5b9eef3e6bd73d8174516b9e4732574e
root <mail@wanguanglu.com>
2017-08-24 13:13:00 +0800

    add object pool.

--- test_boost/pool/object_pool.cpp
+++ test_boost/pool/object_pool.cpp
@@ -24,15 +24,24 @@ struct Student{
 };
 
 int main(int argc, char* argv[]) {
-  boost::object_pool<Student> alloctor;
+  boost::object_pool<Student> alloctor(1, 2);
+  std::cout << alloctor.get_next_size() << std::endl;
   Student* a = alloctor.construct();
+  std::cout << a << "\t" << alloctor.get_next_size() << std::endl;
   Student* b = alloctor.construct();
   alloctor.destroy(a);
   alloctor.destroy(b);
 
+  std::cout << b << "\t" << alloctor.get_next_size() << std::endl;
+
   std::cout << "--------------" << std::endl;
   Student* c = alloctor.malloc();
+  std::cout << c << "\t" << alloctor.get_next_size() << std::endl;
   alloctor.free(c);
+  Student* d = alloctor.construct();
+  std::cout << d << "\t" << alloctor.get_next_size() << std::endl;
+  alloctor.destroy(d);
+
   return 0;
 }
 
