c11985372a644d74d7e8c6f76b4ad73e5e805fbf
root <mail@wanguanglu.com>
2017-10-30 18:45:34 +0800

    add test_shared_ptr.

---
+++ test_c11/test_shared_ptr.cpp
@@ -0,0 +1,42 @@
+/**
+* @file   test_shared_ptr.cpp
+* @author wanguanglu(mail@wanguanglu.com)
+* @date   2017/10/30 18:29:27
+* @brief 
+*  
+**/
+
+#include <iostream>
+#include <memory>
+
+// Basic test.
+void test1() {
+  std::shared_ptr<int> a = std::make_shared<int>(1);
+  std::cout << "a = " << *a << std::endl;
+  std::shared_ptr<int> b = a;
+  std::cout << "use count of a " << a.use_count() << std::endl;
+  std::cout << "a.get() " << a.get() << std::endl;
+  std::cout << "b.get() " << b.get() << std::endl;
+
+  std::shared_ptr<int> c = std::make_shared<int>(*a);
+  std::cout << "use count of a " << a.use_count() << std::endl;
+}
+
+void test2() {
+  std::shared_ptr<int> a = std::make_shared<int>(1);
+  const std::shared_ptr<int>& b = a;
+  std::cout << "use count of a " << a.use_count() << std::endl;
+  *b = 0;
+  std::cout << "a = " << *a << std::endl;
+  std::shared_ptr<int> c = std::make_shared<int>(1);
+  // b = c; // Error
+
+  std::shared_ptr<const int> d = a;
+  std::cout << "use count of a " << a.use_count() << std::endl;
+  // *d = 2;  // Error;
+  d = c;
+}
+
+int main(int argc, char* argv[]) {
+  test2();
+}
